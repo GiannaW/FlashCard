2bc6f33a923497823ddce654029b2d2f0d46365c
Gianna <gw0064a@student.american.edu>
2020-03-21 22:03:47 -0400

    Simple Flashcards lab done

--- FUCKTHISSHIT/ViewController.swift
+++ FUCKTHISSHIT/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var FrontLabel: UILabel!
+    
+    @IBOutlet weak var BackLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func DidOnTapofFlashCard(_ sender: Any) {
+        
+        
+        
+        
+        
+    }
 }
 
